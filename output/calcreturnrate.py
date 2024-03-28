d6e0ab6466547420835c17a5b207d0712b9513d4
Wangyu <pandeskter@gmail.com>
2024-03-29 00:52:40 +0800

    增加运行命令
    计算简单收益率

---
+++ output/calcreturnrate.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+index = pd.read_csv(r'E:\resources\project\bert\bert-master\data_collection\saved\sz399006.csv')
+
+print(index.head)
--- output/dataproc.py
+++ output/dataproc.py
@@ -1,4 +1,4 @@
-# 处理bert预测的概率tsv，将其与原始数据表相结合
+# 处理bert预测的概率tsv，将其与原始数据表相结合，
 
 import pandas as pd
 
@@ -10,11 +10,38 @@ total.pop(total.columns[0])
 prob = pd.read_csv(prob_path, sep='\t')
 
 predicted_labels = prob.idxmax(axis=1)
-print(predicted_labels.head)
+# print(predicted_labels.head)
 
 combined_df = pd.concat([total, predicted_labels], axis=1, ignore_index=False)
 combined_df.columns = ['Upload Time', 'Read', 'Author', 'Title', 'Label']
-print(combined_df.head)
+combined_df.pop('Author')
 
+# combined_df.to_csv(r'\raw.csv')
 
-# print(prob.head)
+# 将字符串日期转换为datetime日期
+combined_df['Upload Time'] = pd.to_datetime(combined_df['Upload Time']).dt.date
+
+# # 使用简单投资者情绪计算方法
+# grouped_df = combined_df.groupby('Upload Time')['Label'].value_counts()
+# grouped_df.to_csv(r'count.csv')
+
+# 使用加权投资者情绪计算方法
+grouped_df = combined_df.groupby(['Upload Time', 'Label'])['Read'].sum().reset_index()
+count_df = combined_df.groupby(['Upload Time'])['Read'].sum().reset_index()
+
+print(count_df.head)
+
+label_map = {'negative': -1, 'neutral': 0, 'positive': 1}
+grouped_df['label_score'] = grouped_df['Label'].map(label_map)
+grouped_df['index'] = grouped_df['label_score'] * grouped_df['Read']
+index_by_date = grouped_df.groupby('Upload Time')['index'].sum()
+
+merged_df = pd.merge(index_by_date, count_df, on='Upload Time')
+
+# print(merged_df.head)
+# merged_df.to_csv(r'merged.csv', index=False)
+
+merged_df['norm index'] = merged_df['index'] / merged_df['Read']
+
+print(merged_df.head)
+merged_df.to_csv(r'merged.csv', index=False)
